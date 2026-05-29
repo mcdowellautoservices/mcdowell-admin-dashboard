53af1f35af37bee53c6f2b2190b51b9625a7912d
Peter McDowell <mcdowellautoservicesltd@gmail.com>
2026-05-29 23:21:53 +0100

    Use direct Firebase config

--- src/firebaseconfig.js
+++ src/firebaseconfig.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyD6aEonsVeyHZ6ZN1oHZUXtSc8o7G-EycM",
+  authDomain: "mcdowell-tyre-app.firebaseapp.com",
+  projectId: "mcdowell-tyre-app",
+  storageBucket: "mcdowell-tyre-app.firebasestorage.app",
+  messagingSenderId: "299380777264",
+  appId: "1:299380777264:web:25b15ac214869cc8a128f5",
+  measurementId: "G-N4P0R0NBVM",
 };
 
 const app = initializeApp(firebaseConfig);
